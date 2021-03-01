82dd85b8ac6c9fb5d714430da03a4ea5dae8dabc
Sevynidd <44946862+Sevynidd@users.noreply.github.com>
2021-03-01 12:10:13 +0100

    Add files via upload

---
+++ src/main/Main.java
@@ -0,0 +1,32 @@
+package main;
+
+import javax.swing.UIManager;
+
+import com.formdev.flatlaf.FlatLightLaf;
+
+import view.fenster.Gegner;
+import view.fenster.Spieler;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+
+			try {
+				UIManager.setLookAndFeel(new FlatLightLaf());
+			} catch (Exception ex) {
+				System.err.println("Failed to initialize LaF");
+			}
+			
+			Spieler spieler = new Spieler();
+			spieler.frame_spieler.setVisible(true);
+
+			Gegner gegner = new Gegner();
+			gegner.frame_gegner.setVisible(true);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
