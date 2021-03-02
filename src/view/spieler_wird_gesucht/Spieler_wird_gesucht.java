7d626789d886e14b972f063cf3b7d27d18cf8215
Sevynidd <44946862+Sevynidd@users.noreply.github.com>
2021-03-02 08:16:28 +0100

    Update Spieler_wird_gesucht.java

--- src/view/spieler_wird_gesucht/Spieler_wird_gesucht.java
+++ src/view/spieler_wird_gesucht/Spieler_wird_gesucht.java
@@ -22,21 +22,21 @@ public class Spieler_wird_gesucht {
 	}
 
 	private void initialize() {
-		
+												//JFrame spieler_wird_gesucht
 		frame_spieler_wird_gesucht = new JFrame();
 		frame_spieler_wird_gesucht.setAlwaysOnTop(true);
 		frame_spieler_wird_gesucht.setResizable(false);
 		frame_spieler_wird_gesucht.setBounds(300, 250, 500, 245);
 		frame_spieler_wird_gesucht.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame_spieler_wird_gesucht.getContentPane().setLayout(null);
-		
+												//JProgressBar
 		progressBar = new JProgressBar();
 		progressBar.setStringPainted(true);
 		progressBar.setLayout(null);
 		progressBar.setBounds(10, 165, 466, 20);
 		progressBar.setValue(0);
 		frame_spieler_wird_gesucht.getContentPane().add(progressBar);
-		
+												//JTextField spieler_wird_gesucht
 		txtSpielerWirdGesucht = new JTextField();
 		txtSpielerWirdGesucht.setHorizontalAlignment(SwingConstants.CENTER);
 		txtSpielerWirdGesucht.setText("Spieler wird gesucht ...");
@@ -46,6 +46,7 @@ public class Spieler_wird_gesucht {
 		txtSpielerWirdGesucht.setFocusable(false);
 		frame_spieler_wird_gesucht.getContentPane().add(txtSpielerWirdGesucht);
 		
+		// Thread wird gestartet um die Progressbar zu animieren
 		Thread_Spieler_wird_gesucht swg = new Thread_Spieler_wird_gesucht();
 		Thread spieler_wird_gesucht = new Thread(swg);
 		spieler_wird_gesucht.start();
